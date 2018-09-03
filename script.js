85ac2f0ddb7c98e4f0010e7f6ce5530dd8e3c627
Carl <cb54@hw.ac.uk>
2018-09-04 00:30:16 +0100

    phq-9 changes

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// onClick color change
+//onClick color change
     function setColor(btn) {
        document.getElementById(btn).style.backgroundColor = "#33B5E5"
        document.getElementById(btn).style.color = "white"       
     }
+
+//allow only 1 answer per question
+function selectOnlyThis(id) {
+    for (var i = 1;i <= 4; i++)
+    {
+        document.getElementById("check" + i).checked = false;
+    }
+    document.getElementById(id).checked = true;
+}
